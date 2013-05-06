63755ba15e076d9024241110b48fead6ef08f991
Clay Miller <clay@smockle.com>
2013-05-06 10:30:42 -0500

    Moved secrets into Figaro. Included config folder.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     
   def index
     @tweet = auto_link(Twitter.user("smockled").status.text)
-    @question = HTTParty.get("https://api.stackexchange.com/2.1/me/questions?order=desc&sort=activity&site=stackoverflow&access_token=" + Smockle::Application.config.so_access_token + "&key=" + Smockle::Application.config.so_key)["items"].first["title"]
-    @image = HTTParty.get("https://api.instagram.com/v1/users/self/media/recent?access_token=" + Smockle::Application.config.igram_access_token)["data"].first["images"]["standard_resolution"]["url"]
+    @question = HTTParty.get("https://api.stackexchange.com/2.1/me/questions?order=desc&sort=activity&site=stackoverflow&access_token=" + Figaro.env.SO_ACCESS_TOKEN + "&key=" + Figaro.env.SO_KEY)["items"].first["title"]
+    @image = HTTParty.get("https://api.instagram.com/v1/users/self/media/recent?access_token=" + Figaro.env.IGRAM_ACCESS_TOKEN)["data"].first["images"]["standard_resolution"]["url"]
   end
 end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Smockle::Application.config.session_store :cookie_store, key: '_smockle_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Smockle::Application.config.session_store :active_record_store
