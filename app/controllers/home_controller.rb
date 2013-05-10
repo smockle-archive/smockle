3e4d078755c61b55b028816e3f234e9d8d4c453f
Clay Miller <clay@smockle.com>
2013-05-10 14:13:21 -0500

    Set :username_include_symbol to linkify Twitter "@"s.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   include HTTParty
     
   def index
-    @tweet = auto_link(Twitter.user("smockled").status.text)
+    @tweet = auto_link(Twitter.user("smockled").status.text, {:username_include_symbol => true})
     @question = HTTParty.get("https://api.stackexchange.com/2.1/me/questions?order=desc&sort=activity&site=stackoverflow&access_token=" + Figaro.env.SO_ACCESS_TOKEN + "&key=" + Figaro.env.SO_KEY)["items"].first["title"]
     @image = HTTParty.get("https://api.instagram.com/v1/users/self/media/recent?access_token=" + Figaro.env.IGRAM_ACCESS_TOKEN)["data"].first["images"]["standard_resolution"]["url"]
   end
