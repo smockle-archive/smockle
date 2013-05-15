1175ab12faebb61d7846ecd2211ddcdc0dc56605
Clay Miller <clay@smockle.com>
2013-05-15 16:50:46 -0500

    Merged work.js and work.less into home.js and home.less.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -55,6 +55,16 @@ SMOCKLE = $.extend(typeof SMOCKLE === "undefined" ? {} : SMOCKLE,
           }
       }
       $(window).on("scroll", scroll);
+    },
+    
+    work: function() {
+      // Error pages only have one section, so disable scrolling.
+      $(document).off("click", "nav a");
+      
+      // Linkify navigation.
+      $("nav a").each(function() {
+        $(this).attr("href", "/" + $(this).attr("href"));
+      });
     }
   }
 });
