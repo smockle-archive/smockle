076740c7b9f791a645e434fd75c50e1adcedd27d
Clay Miller <clay@smockle.com>
2013-06-04 22:33:07 -0500

    Added message table to database. Created tests.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130516175222) do
+ActiveRecord::Schema.define(version: 20130605022324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: true do |t|
     t.string   "name"
--- test/functional/errors_controller_test.rb
+++ test/functional/errors_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class ErrorsControllerTest < ActionController::TestCase
-#  test "should get not_found" do
-#    get :not_found
-#    assert_response :success
-#  end
+  test "should get not_found" do
+    get :not_found
+    assert_response :missing
+  end
 
-#  test "should get server_error" do
-#    get :server_error
-#    assert_response :success
-#  end
+  test "should get server_error" do
+    get :server_error
+    assert_response :error
+  end
 end
