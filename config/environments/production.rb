1a6fbbb5d02ba0f1edf3aff69a096784bb8ead44
Clay Miller <clay@smockle.com>
2013-06-24 20:07:18 -0500

    Fall back to assets pipeline if a precompiled asset is missed.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Smockle::Application.configure do
   config.assets.compress = true
 
   # Fall back to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
