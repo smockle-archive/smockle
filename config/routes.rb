4ff99d701961234382d4066c56bcacb30cec0447
Clay Miller <clay@smockle.com>
2013-06-27 10:40:14 -0500

    Asynchronously load a feeds partial. Closes #6.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Smockle::Application.routes.draw do
 
   get "home/index"
+  get "home/feeds", constraints: { xhr?: true }
   post "/mail" => "home#mail"
   get "/projects(/:id)" => "home#projects"
 
