d7777f695f91bf29e10bbc8305133e0d3aaf1d7f
Clay Miller <clay@smockle.com>
2013-09-13 09:38:47 -0500

    Fix typo.

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,5 +1,5 @@
   AssetSync.configure do |config|
-    config.fog_profider = Figaro.env.FOG_PROVIDER
+    config.fog_provider = Figaro.env.FOG_PROVIDER
     config.fog_directory = Figaro.env.FOG_DIRECTORY
     config.aws_access_key_id = Figaro.env.AWS_ACCESS_KEY_ID
     config.aws_secret_access_key = Figaro.env.AWS_SECRET_ACCESS_KEY
