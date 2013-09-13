d27fa3e8a6adedd9780fbc5d9940b9c0a19683b3
Clay Miller <clay@smockle.com>
2013-09-13 09:14:10 -0500

    Test gzip compression.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Smockle::Application.configure do
   
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.middleware.insert_before Rack::Cache, Rack::Zippy::AssetServer
 end
