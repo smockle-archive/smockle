bcebb0c6312b6e035ce11c3b9a7db136ad560975
Clay Miller <clay@smockle.com>
2013-09-16 23:18:37 -0500

    Check code with JSLint. Closes #8.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -7,110 +7,137 @@
  * Copyright © 2013 Clay Miller (clay@smockle.com)
  */
 
-SMOCKLE = $.extend(typeof SMOCKLE === "undefined" ? {} : SMOCKLE, {
-  home: {
-    index: function() {
-      // Load feeds.
-      $.ajax({
-        url: "/home/feeds",
-        success: function(data) {
-           $("section#feeds").html(data);
-        }
-      });
-      
-      // De-linkify logo.
-      var logo = $(".site-logo");
-      logo.removeAttr("href");
-      logo.children("h1").css("color", "white");
-      
-      // Last section id.
-      var lid;
-      
-      // Collection of sections.
-      var sections = $("nav a").map(function() {
-        var item = $($(this).attr("href"));
-        if (item.length) { return item; }
-      });
-      
-      // Jump to section on click.
-      function click(e) {
-      	$(window).off("scroll");
-      	if (!e) e = window.event;
-      	e.preventDefault();
-      	var h = e.target.href;
-      	var r = h.substring(h.indexOf("#"));
-      	var a = $(r)[0].offsetTop;
-      	var b = $("header")[0].scrollHeight - 4;
-      	var c = a - b > 0 ? a - b : 0;
-		    $("nav a").removeClass("active");
-		    $(e.target).addClass("active");
-		    if (~h.indexOf("#contact")) $("#contact textarea").focus();
-		    else $("#contact textarea").blur();
-      	$("html, body").stop().animate({ scrollTop: c }, 600, function () {$(window).on("scroll", scroll);});
-      }
-      $(document).on("click", "nav a", click);
+/*jslint browser: true, devel: true, bitwise: true */
+/*global $:true */
+var SMOCKLE = $.extend(typeof SMOCKLE === "undefined" ? {} : SMOCKLE, {
+    home: {
+        index: function () {
+            "use strict";
             
-      // Update section on scroll.
-      function scroll() {
-        var b = $("header")[0].scrollHeight;
-        var t = $(this).scrollTop() + b;
-        var c = sections.map(function() {
-          if ($(this).offset().top < t) return this;
-        });
-        c = c[c.length - 1];
-        var id = c && c.length ? c[0].id : "";
-        if (id !== lid) {
-			    lid = id;
-			    $("nav a").removeClass("active");
-			    $("nav a[href=#" + id + "]").addClass("active");
-			    if (lid == "contact") $("#contact textarea").focus();
-			    else $("#contact textarea").blur();
-        }
-      }
-      $(window).on("scroll", scroll);
-	  
-	    // Submit link.
-	    $("#contact a[type=submit]").on("click", function() {
-		    var form = $("#contact form");
-		    var values = form.serialize();
-        $.ajax({
-          url: form.attr("action"),
-          dataType: "json",
-          data: values,
-          type: "POST",
-          success: function (data) {
-            $("#contact h4").text(data.message).css("text-align", "center");
-            $("#contact textarea").hide();
-            $("#contact label").hide();
-          },
-          error: function (data) {
-            var errors = jQuery.parseJSON(data.responseText);
-            console.log(jQuery.parseJSON(data.responseText));
-            if (errors.content != undefined)
-              $("#contact textarea").attr("placeholder", "Content " + errors.content + ".");
-            if (errors.name != undefined)
-              $("#contact label:nth-of-type(1) input").attr("placeholder", "Name " + errors.name + ".");
-            if (errors.email != undefined)
-              $("#contact label:nth-of-type(2) input").attr("placeholder", "Email " + errors.email + ".");
-          }
-        });
-	    });
-    
-      // Safari Hack
-      // Safari rounds in a very special way,
-      // so we must compensate.
-      if (/Constructor/.test(window.HTMLElement))
-        $("#contact a[type=submit]").css("margin", "-2.34rem 0");
-    },
-    
-    projects: function() {
-      // Error pages only have one section, so disable scrolling.
-      $(document).off("click", "nav a");
+            // Load feeds.
+            $.ajax({
+                url: "/home/feeds",
+                success: function (data) {
+                    $("section#feeds").html(data);
+                }
+            });
+          
+            // De-linkify logo.
+            // Last section id.
+            var logo = $(".site-logo"),
+                lid,
+                sections;
+            logo.removeAttr("href");
+            logo.children("h1").css("color", "white");
       
-      // Linkify navigation.
-      $("nav a").each(function() {
-        $(this).attr("href", "/" + $(this).attr("href"));
-      });
+            // Collection of sections.
+            sections = $("nav a").map(function () {
+                var item = $($(this).attr("href"));
+                if (item.length) { return item; }
+            });
+  
+            // Update section on scroll.
+            function scroll() {
+                var b = $("header")[0].scrollHeight,
+                    t = $(window).scrollTop() + b,
+                    c = sections.map(function () {
+                        if ($(this).offset().top < t) {
+                            return this;
+                        }
+                    }),
+                    id;
+                
+                c = c[c.length - 1];
+                id = c && c.length ? c[0].id : "";
+                
+                if (id !== lid) {
+                    lid = id;
+                    $("nav a").removeClass("active");
+                    $("nav a[href=#" + id + "]").addClass("active");
+                    
+                    if (lid === "contact") {
+                        $("#contact textarea").focus();
+                    } else {
+                        $("#contact textarea").blur();
+                    }
+                }
+            }
+            $(window).on("scroll", scroll);
+            
+            // Jump to section on click.
+            function click(e) {
+                $(window).off("scroll");
+                if (!e) { e = window.event; }
+                e.preventDefault();
+                
+                var h = e.target.href,
+                    r = h.substring(h.indexOf("#")),
+                    a = $(r)[0].offsetTop,
+                    b = $("header")[0].scrollHeight - 4,
+                    c = a - b > 0 ? a - b : 0;
+                
+                $("nav a").removeClass("active");
+                $(e.target).addClass("active");
+                
+                if (~h.indexOf("#contact")) {
+                    $("#contact textarea").focus();
+                } else {
+                    $("#contact textarea").blur();
+                }
+                
+                $("html, body").stop().animate({ scrollTop: c }, 600, function () { $(window).on("scroll", scroll); });
+            }
+            $(document).on("click", "nav a", click);
+	  
+            // Submit link.
+            $("#contact a[type=submit]").on("click", function () {
+                var form = $("#contact form"),
+                    values = form.serialize();
+                
+                $.ajax({
+                    url: form.attr("action"),
+                    dataType: "json",
+                    data: values,
+                    type: "POST",
+                    success: function (data) {
+                        $("#contact h4").text(data.message).css("text-align", "center");
+                        $("#contact textarea").hide();
+                        $("#contact label").hide();
+                    },
+                    error: function (data) {
+                        var errors = $.parseJSON(data.responseText);
+                        console.log($.parseJSON(data.responseText));
+                        
+                        if (errors.content !== undefined) {
+                            $("#contact textarea").attr("placeholder", "Content " + errors.content + ".");
+                        }
+                        if (errors.name !== undefined) {
+                            $("#contact label:nth-of-type(1) input").attr("placeholder", "Name " + errors.name + ".");
+                        }
+                        if (errors.email !== undefined) {
+                            $("#contact label:nth-of-type(2) input").attr("placeholder", "Email " + errors.email + ".");
+                        }
+                    }
+                });
+            });
+    
+            // Safari Hack
+            // Safari rounds in a very special way, so we must compensate.
+            if (/Constructor/.test(window.HTMLElement)) {
+                $("#contact a[type=submit]").css("margin", "-2.34rem 0");
+            }
+        },
+    
+        projects: function () {
+            "use strict";
+            
+            // Error pages only have one section, so disable scrolling.
+            $(document).off("click", "nav a");
+      
+            // Linkify navigation.
+            $("nav a").each(function () {
+                $(this).attr("href", "/" + $(this).attr("href"));
+            });
+        }
     }
-  }
 });
