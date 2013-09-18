50607eb7108afd07c99fee6fec00128b486e9842
Clay Miller <clay@smockle.com>
2013-09-18 04:41:08 -0500

    Don't overwrite existing JavaScript functions.

--- app/assets/javascripts/ready.js
+++ app/assets/javascripts/ready.js
@@ -11,35 +11,36 @@ if (window.$ === undefined) {
     window.$ = {};
 }
 
-$.ready = function (fn) {
-    "use strict";
-    
-    var DOMContentLoaded;
-    
-    /* DOM loading in modern browsers. */
-    if (document.addEventListener) {
-        DOMContentLoaded = function () {
-            document.removeEventListener("DOMContentLoaded", DOMContentLoaded, false);
-            fn();
-        };
-        document.addEventListener("DOMContentLoaded", DOMContentLoaded, false);
-        window.addEventListener("load", fn, false);
-    
-    /* DOM loading in IE. */
-    } else if (document.attachEvent) {
-        DOMContentLoaded = function () {
-            if (document.readyState === "complete") {
-                document.detachEvent("onreadystatechange", DOMContentLoaded);
+if ($.ready === undefined) {
+    $.ready = function (fn) {
+        "use strict";
+        
+        var DOMContentLoaded;
+        
+        /* DOM loading in modern browsers. */
+        if (document.addEventListener) {
+            DOMContentLoaded = function () {
+                document.removeEventListener("DOMContentLoaded", DOMContentLoaded, false);
                 fn();
-            }
-        };
-        document.attachEvent("onreadystatechange", DOMContentLoaded);
-        window.attachEvent("onload", fn);
-    }
-    
-    /* DOM already loaded. */
-    if (document.readyState === "complete") {
-        setTimeout(fn, 1);
-    }
-};
-
+            };
+            document.addEventListener("DOMContentLoaded", DOMContentLoaded, false);
+            window.addEventListener("load", fn, false);
+        
+        /* DOM loading in IE. */
+        } else if (document.attachEvent) {
+            DOMContentLoaded = function () {
+                if (document.readyState === "complete") {
+                    document.detachEvent("onreadystatechange", DOMContentLoaded);
+                    fn();
+                }
+            };
+            document.attachEvent("onreadystatechange", DOMContentLoaded);
+            window.attachEvent("onload", fn);
+        }
+        
+        /* DOM already loaded. */
+        if (document.readyState === "complete") {
+            setTimeout(fn, 1);
+        }
+    };
+}
