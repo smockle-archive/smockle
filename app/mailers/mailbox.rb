8683482ad82e0ad912310bd382109787f2ed4d7c
Clay Miller <clay@smockle.com>
2013-09-26 11:48:58 -0500

    Change default from address for contact emails. Fixes #9.

--- app/mailers/mailbox.rb
+++ app/mailers/mailbox.rb
@@ -1,6 +1,6 @@
 class Mailbox < ActionMailer::Base
   default to: Figaro.env.GMAIL_USERNAME
-  default from: Figaro.env.GMAIL_USERNAME
+  default from: Figaro.env.GMAIL_ALIAS
   
   def contact(message)
     @message = message
