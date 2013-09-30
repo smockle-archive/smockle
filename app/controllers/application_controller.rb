a158e769bc5015031eb4567066be4b8829e72f81
Clay Miller <clay@smockle.com>
2013-09-30 03:36:25 -0500

    Loosen Content Security Policy so that Google Analytics and New Relic load on the staging server. #33.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :set_csp
 
   def set_csp
-    policy = "default-src 'self'; script-src 'self' ajax.googleapis.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' *.s3.amazonaws.com; font-src 'self' themes.googleusercontent.com"
+    policy = "default-src 'self' *.smockle.com; script-src 'self' 'unsafe-inline' *.smockle.com ajax.googleapis.com; style-src 'self' 'unsafe-inline' *.smockle.com fonts.googleapis.com; img-src 'self' *.smockle.com *.s3.amazonaws.com; font-src 'self' *.smockle.com themes.googleusercontent.com"
     response.headers['Content-Security-Policy'] = policy
     response.headers['X-Content-Security-Policy'] = policy
     response.headers['X-WebKit-CSP'] = policy
