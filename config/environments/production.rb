09c906135c9f094669a689de1023f59e85aa971f
Clay Miller <clay@smockle.com>
2014-07-14 04:15:19 -0500

    Specify js_compressor and css_compressor per http://www.andylindeman.com/2013/05/08/my-assets-are-no-longer-compressed-in-rails-4-halp.html.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,8 @@ Smockle::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.js_compressor  = :uglifier
+  config.assets.css_compressor = :sass
 
   # Fall back to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
